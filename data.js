de68dc7e3cc12d28f3ab20268d90522c5d209345
comunnidadoscar@gmail.com <comunidadoscar@gmail.com>
2021-12-16 15:48:56 -0500

    added journals

--- data.js
+++ data.js
@@ -1,70 +1,77 @@
-let slots = {
-    projects:[
+var slots = {
+    projects: [
         //index:0
         {
-            name:"Black ops Fanpage",
-            pic:"Screenshot (9).png",
-            link:"https://skymazter.github.io/fanpage/"
+            name: "Black ops Fanpage",
+            pic: "Screenshot (9).png",
+            link: "https://skymazter.github.io/fanpage/"
         },
         {
-            name:"Rock, Paper, Scissors",
-            pic:"Screenshot (10).png",
-            link:"https://skymazter.github.io/rockpaperscissors/"
+            name: "Rock, Paper, Scissors",
+            pic: "Screenshot (10).png",
+            link: "https://skymazter.github.io/rockpaperscissors/"
         },
         {
-            name:"Personal Playlist",
-            pic:"Screenshot (13).png",
-            link:"https://skymazter.github.io/playlist/"
+            name: "Personal Playlist",
+            pic: "Screenshot (13).png",
+            link: "https://skymazter.github.io/playlist/"
         },
         {
-            name:"Buzzfeed Quiz",
-            pic:"Screenshot (11).png",
-            link:"https://skymazter.github.io/quiz/"
+            name: "Buzzfeed Quiz",
+            pic: "Screenshot (11).png",
+            link: "https://skymazter.github.io/quiz/"
         },
         {
-            name:"Giphy",
-            pic:"Screenshot (14).png",
-            link:"https://skymazter.github.io/giphy/"
+            name: "Giphy",
+            pic: "Screenshot (14).png",
+            link: "https://skymazter.github.io/giphy/"
         },
         {
-            name:"My Weather Teller",
-            pic:"Screenshot (1).png",
-            link:"https://skymazter.github.io/weather-endgame/"
+            name: "My Weather Teller",
+            pic: "Screenshot (1).png",
+            link: "https://skymazter.github.io/weather-endgame/"
         },
         {
-            name:"Chemistry Guru",
-            pic:"Screenshot (16).png",
-            link:""
+            name: "Chemistry Guru",
+            pic: "Screenshot (16).png",
+            link: "https://skymazter.github.io/chemistryProject/"
         },
         {
-            name:"",
-            pic:"",
-            link:""
+            name: "",
+            pic: "",
+            link: ""
         },
         {
-            name:"",
-            pic:"",
-            link:""
+            name: "",
+            pic: "",
+            link: ""
         },
         {
-            name:"",
-            pic:"",
-            link:""
+            name: "",
+            pic: "",
+            link: ""
         },
         {
-            name:"",
-            pic:"",
-            link:""
+            name: "",
+            pic: "",
+            link: ""
         },
         {
-            name:"",
-            pic:"",
-            link:""
+            name: "",
+            pic: "",
+            link: ""
         },
         {
-            name:"",
-            pic:"",
-            link:""
-        },
+            name: "",
+            pic: "",
+            link: ""
+        }
     ]
-}
+};
+var journals = {
+    entries: [
+        {
+            link: ""
+        }
+    ]
+};
--- node.js
+++ node.js
@@ -1,15 +1,82 @@
-let datalength = slots.projects.length;
-slots.projects.name;
+//DOM constructor
+// new Document();
+
+var datalength = slots.projects.length;
+
+var numJournals = 0;
 console.log(datalength);
+
+//code to pasting the project data from the object to the page
 function pasteProjects(){
     for( i = 0; i < datalength; i+=1){
-        $("#projects").append("<div class = 'project'>"+
-        "<img src='"+slots.projects[i].pic+"'>"+
-        "<div class = 'side_info'>"+
-        "<h1>"+slots.projects[i].name+"</h1>"+
-        "<a href='"+slots.projects[i].link+"'>Project Availabe Here</a>"+
-        "</div>"+
-        "</div>");
+        if(slots.projects[i].name != ""){
+            $("#projects").append("<div class = 'project'>"+
+            "<img src='"+slots.projects[i].pic+"'>"+
+            "<div class = 'side_info'>"+
+            "<h1>"+slots.projects[i].name+"</h1>"+
+            "<a href='"+slots.projects[i].link+"'>Project Availabe Here</a>"+
+            "</div>"+
+            "</div>");
+        }
     };
 };
 pasteProjects();
+
+//switches from displaying cs projects to displaying journalism stuff
+
+var sub_title = document.querySelector(".sub-title");
+var portfolio_content = document.querySelector("#projects");
+var main = document.querySelector("#main");
+
+
+function pasteJournals(){
+    $("#projects").html("<embed class = 'journals' src='profile.pdf' width='800px' height='700px' />"
+    + "<embed class = 'journals' src='QandA.pdf' width='800px' height='700px' />");
+
+    sub_title.innerHTML = "<h1 class>Journals</h1>";
+    document.getElementById("header").appendChild(btn);
+};
+//Show journalism stuff
+$("#postJourn").click(function(){
+    pasteJournals();
+});
+var journ = document.createElement("button");
+    journ.innerHTML = "Journalism";
+    journ.id = "postJourn";
+    journ.addEventListener("click", function(){
+        pasteJournals();
+    });
+
+//return to CS
+var btn = document.createElement("button");   // Create a <button> element
+    btn.innerHTML = "Coding";                   // Insert text
+    btn.id = "postCs";
+    btn.addEventListener("click", function(){
+        $("#projects").html("");
+        document.getElementById("header").removeChild(btn);
+        document.getElementById("header").innerHTML = "<h1>CSS - HTML - Javascript</h1>";
+        // $("#header").append("<button id  = 'postJourn'>Clear</button>");
+        pasteProjects();
+        document.getElementById("header").appendChild(journ);
+        console.log("done");
+    });
+
+
+
+
+
+//contact page buttons
+
+
+$("#insta").click(function(){
+    window.open("https://www.instagram.com/kevincomunidad/");
+});
+$("#linkedIn").click(function(){
+    window.open("https://www.linkedin.com/in/oscar-comunidad-90b224168/");
+});
+$("#twitch").click(function(){
+    window.open("https://www.twitch.tv/a_little_syco");
+});
+$("#github").click(function(){
+    window.open("https://github.com/SkyMazter");
+});
