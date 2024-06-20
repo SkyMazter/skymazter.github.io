41e4f9df59654b30684800dfe3f8fe18ad17da6a
SkyMazter <comunidadoscar@gmail.com>
2024-06-20 15:34:36 -0400

    finished landing page

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,24 +1,66 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
+import P5Background from "./components/P5Background";
 const App = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h2>Oscar .K. Comunidad</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button>Portfolio</Button>
-        </Col>
-        <Col>
-          <Button>About Me</Button>
-        </Col>
-        <Col>
-          <Button>Contact Me</Button>
-        </Col>
-      </Row>
-    </Container>
+    <div
+      className="App"
+      style={{
+        position: "relative",
+        height: "100vh",
+        width: "100vw",
+        overflow: "hidden",
+      }}
+    >
+      <P5Background />
+      <div
+        style={{
+          position: "relative",
+          height: "100vh",
+          width: "100vw",
+          zIndex: 10,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Container className="">
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <h1 className="fw-bolder" style={{
+                fontFamily: "Karla, sans-serif",
+                fontWeight: "200",
+                fontStyle: "normal",
+                fontSize: "75px"
+              }}>Oscar .K. Comunidad</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <p>
+                Software Developer | Photographer | Digital Justice Advocate
+              </p>
+            </Col>
+          </Row>
+          {/* <Row>
+            <Col className="d-flex justify-content-center">
+              <p>Message of the day</p>
+            </Col>
+          </Row> */}
+          <Row>
+            <Col className="d-flex justify-content-center">
+              <Button variant="outline-danger">Portfolio</Button>
+            </Col>
+            <Col className="d-flex justify-content-center">
+              <Button variant="outline-primary">About Me</Button>
+            </Col>
+            <Col className="d-flex justify-content-center">
+              <Button variant="outline-success" >
+                Contact Me
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
 
---
+++ frontEnd/src/components/P5Background.tsx
@@ -0,0 +1,100 @@
+import { useRef, useEffect } from "react";
+import p5 from "p5";
+
+const P5Background = () => {
+  const sketchRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const sketch = (p: p5) => {
+      let circles: Circle[] = [];
+      let colors: string[] = ["red", "green", "blue"];
+      // let color = p.random(colors);
+      class Circle {
+        x: number;
+        y: number;
+        diameter: number;
+        speedX: number;
+        speedY: number;
+        color: string;
+
+        constructor() {
+          this.x = p.random(p.width);
+          this.y = p.random(p.height);
+          this.diameter = p.random(20, 50);
+          this.speedX = p.random(-2, 2);
+          this.speedY = p.random(-2, 2);
+          this.color = p.random(colors);
+        }
+
+        move() {
+          this.x += this.speedX;
+          this.y += this.speedY;
+
+          if (this.x > p.width || this.x < 0) {
+            this.speedX *= -1;
+          }
+          if (this.y > p.height || this.y < 0) {
+            this.speedY *= -1;
+          }
+        }
+
+        display() {
+          p.noStroke();
+
+          if (this.color == "red") {
+            p.fill(255, 113, 141, 225);
+          } else if (this.color == "blue") {
+            p.fill(168, 100, 253, 150);
+          } else {
+            p.fill(120, 255, 68, 150);
+          }
+          
+          p.ellipse(this.x, this.y, this.diameter);
+        }
+      }
+
+      p.setup = () => {
+        const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+        canvas.parent(sketchRef.current!);
+        for (let i = 0; i < 100; i++) {
+          circles.push(new Circle());
+        }
+      };
+
+      p.draw = () => {
+        p.background(500, 500, 500);
+        for (let circle of circles) {
+          circle.move();
+          circle.display();
+        }
+      };
+
+      p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+      };
+    };
+
+    const myP5 = new p5(sketch);
+
+    return () => {
+      myP5.remove();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={sketchRef}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: 1,
+        pointerEvents: "none",
+      }}
+    />
+  );
+};
+
+export default P5Background;
