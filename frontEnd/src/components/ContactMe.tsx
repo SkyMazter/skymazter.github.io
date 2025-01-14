82440e91788cafcff3071d91a4ce222ab3a58735
SkyMazter <comunidadoscar@gmail.com>
2025-01-14 00:42:54 -0500

    finished contact me component

--- frontEnd/src/components/ContactMe.tsx
+++ frontEnd/src/components/ContactMe.tsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BsGithub, BsLinkedin, BsInstagram, BsYoutube } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 interface Props {
   contactRef: React.RefObject<HTMLDivElement>;
@@ -9,56 +10,39 @@ interface Props {
 
 const ContactMe = ({ contactRef }: Props) => {
   return (
-    <Container ref={contactRef}>
+    <Container ref={contactRef} className="py-3">
       <Row>
         <Col xs={12} className="border-2 border-dark border-bottom">
           <h2 className=" typewriter karla">Find Me Here</h2>
         </Col>
       </Row>
       <Row>
-        <Col xs={3} className="centerThis">
-          <div
-            className="centerThis"
-            style={{
-              height: "90%",
-              width: "90%",
-            }}
-          >
+        {/* <Col className="centerThis py-3" xs={3} lg={3}>
+          <BsGithub className="logo" />
+        </Col>
+        <Col className="centerThis py-3" xs={3} lg={3}>
+          <BsLinkedin className="logo" />
+        </Col>
+        <Col className="centerThis py-3" xs={3} lg={3}>
+          <BsInstagram className="logo" />
+        </Col>
+        <Col className="centerThis py-3" xs={3} lg={3}>
+          <BsYoutube className="logo" />
+        </Col> */}
+        <Col className="d-flex align-items-center justify-content-between p-3 my-3">
+          <Link to={"https://github.com/SkyMazter"}>
             <BsGithub className="logo" />
-          </div>
-        </Col>
-        <Col xs={3} className="centerThis">
-          <div
-            className="centerThis"
-            style={{
-              height: "90%",
-              width: "90%",
-            }}
-          >
+          </Link>
+          <Link to={"https://www.linkedin.com/in/oscar-comunidad-90b224168/"}>
             <BsLinkedin className="logo" />
-          </div>
-        </Col>
-        <Col xs={3} className="centerThis">
-          <div
-            className="centerThis"
-            style={{
-              height: "90%",
-              width: "90%",
-            }}
-          >
+          </Link>
+          <Link to={"https://www.instagram.com/kevincomunidad/"}>
             <BsInstagram className="logo" />
-          </div>
-        </Col>
-        <Col xs={3} className="centerThis">
-          <div
-            className="centerThis"
-            style={{
-              height: "90%",
-              width: "90%",
-            }}
-          >
-            <BsYoutube className="logo" />
-          </div>
+          </Link>
+          <Link to="https://www.youtube.com/@xenon4916">
+            {" "}
+            <BsYoutube className="logo" />{" "}
+          </Link>
         </Col>
       </Row>
     </Container>
